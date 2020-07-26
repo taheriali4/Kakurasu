17374bba32576c33f30cdb37bd3c7b9bc1721ad5
Richard Yu <richardyu042@gmail.com>
2020-07-26 15:34:41 -0400

    finished the refinement - infinite loop issue?

--- src/kakSolver/kakSolver.hs
+++ src/kakSolver/kakSolver.hs
@@ -14,6 +14,8 @@ values = [True, False]
 --puzzle = [[True,False],
          -- [False,True]]
 
+puzzle :: Question
+puzzle = ([14,10,10,10,77],[6,8,13,12,6])
 
 valid :: Question -> Grid-> Bool
 valid (q1, q2) g = (sumcheck (rows g) q1) && (sumcheck (cols g) q2)
@@ -64,12 +66,12 @@ solveBrute question= filter (valid  question) (collapse (choices (length (fst qu
 
 
 --now we want to prune the search tree a little
-prune :: Matrix Choices -> Matrix Choices
-prune = pruneBy rows . pruneBy cols
+prune :: Question -> Matrix Choices -> Matrix Choices
+prune q m = pruneBy rows( pruneBy cols m (fst q)) (snd q)
 --prune x = pruneBy rows (pruneBy cols x)
         --where pruneBy f = f . map reduce . f
         --where pruneBy f x = f (map reduce (f x)
-        where pruneBy f x = f (map (reduce (sumSingle 1)) (f x))
+        where pruneBy f x q = f (map reduce (zip (f x) q))
 
 {-
 reduce :: Row Choices -> Row Choices 
@@ -78,16 +80,16 @@ reduce xss = [xs `minus` singles | xs <- xss]
 -}
 
 --sets parts of Row Choices as single if theyre greater than the number
-reduce :: Int -> Row Choices -> Row Choices
-reduce num  xss = [if i > num then [False] else [True,False] | (i,s) <- zip [1..] xss]
+reduce ::  (Row Choices, Int) -> Row Choices
+reduce (xss,num) = [if i > (num - sumSingle xss) then [False] else [True,False] | (i,s) <- zip [1..] xss]
 
 --sums up the current single squares in a row
 sumSingle :: Row Choices -> Int
 sumSingle r = fst (foldl (\(acc, num) x -> if (single x) && (head x) then (acc + num, num + 1) else (acc, num + 1)) (0,1) r)
 
 --sums up single squares and unknowns.
---sumUnknowns :: Row Choices -> Int
---sumUnknowns = foldl(\(acc,num) x -> if (not (single x)) || (head x) then (acc + num, num + 1) else (acc, num + 1)) (0,1) 
+sumUnknowns :: Row Choices -> Int
+sumUnknowns = fst . foldl(\(acc,num) x -> if (not (single x)) || (head x) then (acc + num, num + 1) else (acc, num + 1)) (0,1) 
 
 --if the unknowns sum to the number exactly then it has to be true
 --setTrue :: Row Choices -> Int -> Row Choices
@@ -107,28 +109,27 @@ fix f x = if x == x' then x else fix f x'
     where x' = f x
 
 solveFixPrune :: Question -> [Grid]
-solveFixPrune q= filter (valid q) (collapse (fix prune (choices (length (fst q)))))
+solveFixPrune q= filter (valid q) (collapse (fix (prune q) (choices (length (fst q)))))
 
 --turns out that its not that good - it doesnt do anything!
 --we need to take out anything that 
 --lets keep optimizing
-{-
 solve :: Question -> [Grid]
-solve = search prune choices . length . fst 
+solve q = search q (fix (prune q) (choices (length  (fst  q))))
 
-search :: Matrix Choices -> [Grid]
-search m
-    | blocked m = []
+search :: Question -> Matrix Choices -> [Grid]
+search q m
+    | blocked m q = []
     | complete m = collapse m
     | otherwise = [g | m' <- guesses m
-                     , g <- search (prune m')]
+                     , g <- search q (prune q m')]
 
 
 guesses :: Matrix Choices -> [Matrix Choices]
 guesses m = 
     [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c<- cs]
     where
-        (rows1, row : row2) = break (any (not . single)) m --breaks up the rows into 3 parts
+        (rows1, row : rows2) = break (any (not . single)) m --breaks up the rows into 3 parts
         (row1, cs : row2) = break (not . single) row -- breaks up the row into 3 parts
     
 --this is a 'complete square'
@@ -136,8 +137,8 @@ complete :: Matrix Choices -> Bool
 complete = all (all single)
 
 -- 'impossible solution' might not be complete
-blocked :: Matrix Choices -> Bool
-blocked m = void m || not (safe m)
+blocked :: Matrix Choices -> Question -> Bool
+blocked m q = void m || not (safe m q)
 
 --checks if anylists are empty null == true on []
 void :: Matrix Choices -> Bool
@@ -145,11 +146,14 @@ void = any (any null)
 
 --check that we are not going over the sums
 safe :: Matrix Choices -> Question -> Bool
-safe cm q = all consistent (rows cm) (fst q) &&
-            all consistent (cols cm) (fst q)
+safe cm q = all consistent (zip (rows cm) (fst q)) &&
+            all consistent (zip (cols cm) (snd q)) 
 
 --if the ones we know about are 
-consistent :: Row Choices -> [b] -> Bool
+consistent :: (Row Choices, Int) -> Bool
+consistent (r, q) = (sumSingle r) < q
+--want to add up all the singles and then prove that theyre less than the sum
 
--}
 
+main :: IO ()
+main = putStrLn $ show (head  (solve puzzle))
